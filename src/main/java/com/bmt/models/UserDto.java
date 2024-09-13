c9df453ab8dcf2df615a63a586425e049ed72913
Ram Kumar <ram8006357434@gmail.com>
2024-09-13 17:42:45 +0530

    Add files via upload

---
+++ src/main/java/com/bmt/models/UserDto.java
@@ -0,0 +1,40 @@
+/*package com.bmt.Crud.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+public class UserDto {
+
+	@NotEmpty(message = "The First is required")
+	private String userName;
+	@NotEmpty(message = "The password is required")
+	private String password;
+	@NotEmpty(message = "The email is required")
+	@Email
+	private String email;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setpassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
+
+	
+
+*/
